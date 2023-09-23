6013b05568296f81395bcaac6fac8f068d264f26
jishnunpp <jishnunp397@gmail.com>
2023-09-23 14:12:00 +0530

    video details added

--- src/components/Watchpage.js
+++ src/components/Watchpage.js
@@ -2,34 +2,25 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appslice";
 import { useSearchParams } from "react-router-dom";
-import { COMMENT_API_KEY } from "../utils/constant";
+
 import CommentContainer from "./CommentContainer";
+import VideoDetails from "./VideoDetails";
 
 const Watchpage = () => {
   const [searchparams] = useSearchParams();
-  console.log(searchparams.get("v"));
+  // console.log(searchparams.get("v"));
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(closeMenu());
   }, []);
 
-  // useEffect(() => {
-  //   getComment();
-  // }, []);
-
-  // const getComment = async () => {
-  //   const data = await fetch(COMMENT_API_KEY);
-  //   const json = await data.json();
-  //   // console.log(json.items);
-
-  //   console.log(json);
-  // };
+ 
   return (
     <div className="p-4">
       <iframe
-        width="900"
-        height="350"
+        width="950"
+        height="450"
         src={"https://www.youtube.com/embed/" + searchparams.get("v")}
         title="YouTube video player"
         frameBorder="0"
@@ -41,6 +32,7 @@ const Watchpage = () => {
         {"http://www.youtube.com/channel/"+searchparams.get("v")}
       </div> */}
       <div >
+        <VideoDetails/>
         <CommentContainer/>
       </div>
     </div>
